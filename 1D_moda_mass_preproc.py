310e1cf882549453a4ccc12c36371be3969467d1
Marius <marius@pop-os.localdomain>
2023-05-05 18:34:35 +0200

    fixed cut spindles, fixed counter placement

--- 1D_moda_mass_preproc.py
+++ 1D_moda_mass_preproc.py
@@ -6,7 +6,7 @@ MASS_sampling_freq = 256
 
 MASS_path = '/home/marius/Documents/THESIS/data/mass'
 MODA_path = '/home/marius/Documents/THESIS/data/MODA_GC/output/exp/annotFiles'
-MASS_MODA_proccessed_path = '/home/marius/Documents/THESIS/data'
+MASS_MODA_proccessed_path = '/home/marius/Documents/THESIS/data/temp'
 
 from scipy import signal
 from scipy.fft import fftshift
@@ -139,27 +139,50 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=2):
 def overlapping_windows(sequence, labels, master_start, master_stop, sampling_frequency, window_duration, overlap):
     window_len = sampling_frequency * window_duration
     step_size = (1-overlap) * window_len
-    no_windows = int(sequence.shape[0]/step_size)
-    #print(window_len)
-    #print(step_size)
-    #print(no_windows)
+
+    # Additinal seq length is given, cause it's hard to divide a seq into equal lengths. 
+    if ((master_stop-master_start)*sampling_frequency) % step_size == 0:
+        no_windows = int((master_stop-master_start)*sampling_frequency/step_size)
+    else:
+        no_windows = (master_stop-master_start) * sampling_frequency // step_size
+        no_windows += 1
+
     sequence_windowed = []
     labels_windowed = []
     for i in range(0,no_windows):
         window_start = int((i)*step_size)
-        if window_start > ((master_stop-master_start)*sampling_frequency - window_len):
+        if window_start >= ((master_stop-master_start)*sampling_frequency):
             continue
         sequence_windowed.append(sequence[window_start:(window_start+window_len)])
 
         current_window = []
+        #print('WINDOW START')
+        #print(master_start + (window_start/256))
         for j in range(0,len(labels)):
-            # NEEDS TO BE CHANGED. If spindle more than 0.5s then keep
-            if (labels[j][0] - master_start < (window_start+window_len)/sampling_frequency) and (labels[j][0] - master_start > window_start/sampling_frequency):
-                x1 = (labels[j][0]-(window_start/sampling_frequency) - master_start)/window_duration
-                y1 = 0
-                x2 = (labels[j][0]-(window_start/sampling_frequency) - master_start + labels[j][1])/window_duration
-                y2 = 1
+            # if the spindles' end is inside the window and if the spindle starts before current window end
+            if ((labels[j][0] + labels[j][1]) > (master_start + (window_start/sampling_frequency))) and (labels[j][0] < (master_start + window_duration + (window_start/sampling_frequency))):
+                #print('SPINDLE')
+                #print(labels[j][0])
+                
+                if ((labels[j][0] + labels[j][1]) - (master_start + (window_start/sampling_frequency))) < 0.5:
+                    continue
+                # if spindle starts before window start use window start as it's starting point
+                if labels[j][0] < (master_start + (window_start/sampling_frequency)):
+                    x1 = 0
+                    x2 = (labels[j][0] + labels[j][1] - (master_start + (window_start/sampling_frequency)))/window_duration
+                    current_window.append((x1, x2))
+                    continue
+                # if spindle ends after windows end, use windows end as ending coordinate
+                if (labels[j][0] + labels[j][1]) > (master_start + window_duration + (window_start/sampling_frequency)):
+                    x1 = (labels[j][0] - (master_start + (window_start/sampling_frequency)))/window_duration
+                    x2 = 1
+                    current_window.append((x1, x2))
+                    continue
+                
+                x1 = (labels[j][0] - (master_start + (window_start/sampling_frequency)))/window_duration
+                x2 = (labels[j][0] + labels[j][1] - (master_start + (window_start/sampling_frequency)))/window_duration
                 current_window.append((x1, x2))
+        #print(current_window)
         labels_windowed.append(current_window)
     return sequence_windowed, labels_windowed
 
@@ -177,10 +200,10 @@ def get_segment_viewed(mass_recordings_dict, moda_annotation_path, processed_pat
     recording = mne.io.read_raw_edf(recording_path)
     raw_data = recording.get_data()
     c3_channel = raw_data[6,:]
-    
-    for start, duration in zip(relevant_segments.startSec, relevant_segments.durationSec):
-        ADDITIONAL_END = 15
 
+    counter = 0
+    for start, duration in zip(relevant_segments.startSec, relevant_segments.durationSec):
+        ADDITIONAL_END = WINDOW_SIZE
         sequence = c3_channel[int(start*MASS_sampling_freq) : int((start + duration + ADDITIONAL_END)*MASS_sampling_freq)]
 
 
@@ -189,9 +212,8 @@ def get_segment_viewed(mass_recordings_dict, moda_annotation_path, processed_pat
 
         sequence_windowed, labels_windowed = overlapping_windows(sequence, labels.to_numpy(), start, start + duration, MASS_sampling_freq, WINDOW_SIZE, OVERLAP)
 
-
-        counter = 0
         for j,window in enumerate(sequence_windowed):
+           
             if labels_windowed[j] == []:
                 continue
             #fig, Sxx = plot_spectrogram(window, MASS_sampling_freq, win_sec = 2, fmin = 0.3, fmax = 20,train=True, arrays=True)
@@ -211,9 +233,9 @@ def get_segment_viewed(mass_recordings_dict, moda_annotation_path, processed_pat
             if not exists(processed_path + '/1D_MASS_MODA_processed' + '/labels/' + file_name):
                 mkdir(processed_path + '/1D_MASS_MODA_processed' + '/labels/' + file_name)
 
-
             np.save(processed_path + '/1D_MASS_MODA_processed' + '/input/' + file_name + "/" + str(counter) + '.npy', np.asarray(window))
             #np.save(Dreams_path + '/windowed' + '/labels/' + str(i) + "/" + str(j) + '.npy', label_windows)
+
             with open(processed_path + '/1D_MASS_MODA_processed' + '/labels/' + file_name + "/" + str(counter) + '.json', 'w') as fp:
                 json.dump({'boxes':labels_windowed[j], 'labels':[0]*len(labels_windowed[j])}, fp)
 
@@ -237,5 +259,6 @@ for root, dirs, files in walk(MODA_path, topdown=False):
             try:
                 get_segment_viewed(PSG_recordings, join(root,name), MASS_MODA_proccessed_path)
             except:
-                print(name, ' did not load')
+                ...
+                #print(name, ' did not load')
    
